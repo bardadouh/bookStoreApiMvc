3ddfdb4862bf49f9a39fac788242e032a51585cf
BARDADOUH <issam.bardadouh21@ump.ac.ma>
2024-12-10 10:05:50 +0100

    code v3 without jwt auth

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 // const http = require("http");
 const booksPath = require("./routes/books");
 const authorsPath = require("./routes/authors");
+const authPath = require("./routes/auth");
 const mongoose = require("mongoose");
+const logger = require("./middlewares/logger");
+
+const { notFound, errorHandler} = require("./middlewares/errors");
 const dotenv = require("dotenv");
 dotenv.config();
 // connection to db
-mongoose.connect(process.env.MONGO_URI)
-
-// check connection
-.then(() => console.log("Connected to MongoDB..."))
-.catch((error) => console.log("Connection Failed! TO MongoDB: ", error));
+mongoose
+  .connect(process.env.MONGO_URI)
+  // check connection
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((error) => console.log("Connection Failed! TO MongoDB: ", error));
 
 const express = require("express");
+
 //Init App
 const app = express();
 
 // Apply Middleware
 app.use(express.json());
+app.use(logger);
 
 // Routes
 app.use("/api/books", booksPath);
 app.use("/api/authors", authorsPath);
+app.use("/api/auth", authPath);
+
+//Error Handler
+app.use(notFound);
+app.use(errorHandler);
+
 //runing server
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server is running in${process.env.NODE_ENV} on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is running in${process.env.NODE_ENV} on port ${PORT}`)
+);
---
+++ models/User.js
@@ -0,0 +1,88 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+// User Schema
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 5,
+      maxlength: 255,
+      validate: {
+        validator: (value) =>
+          /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(value),
+        message: "{VALUE} is not a valid email address",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      trim: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    username: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+
+// Validate Registration User
+function validateRegistrationUser(user) {
+    const schema = Joi.object({
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(100).required().email(),
+        password: Joi.string().min(8).required().trim(),
+        username: Joi.string().min(3).max(20).required().trim(),
+        isAdmin: Joi.boolean(),
+    });
+    return schema.validate(user);
+}
+// Validate Login User
+
+function validateLoginUser(user) {
+    const schema = Joi.object({
+        email: Joi.string().min(5).max(100).required().email(),
+        password: Joi.string().min(8).required().trim(),
+    });
+    return schema.validate(user);
+}
+
+// Validate Update User
+
+function validateUpdateUser(user) {
+    const schema = Joi.object({
+        name: Joi.string().min(5).max(50),
+        email: Joi.string().min(5).max(100).email(),
+        password: Joi.string().min(8).trim(),
+        username: Joi.string().min(3).max(20).trim(),
+        isAdmin: Joi.boolean(),
+    });
+    return schema.validate(user);
+}
+
+module.exports = {
+    User,
+    validateRegistrationUser,
+    validateLoginUser,
+    validateUpdateUser,
+}
